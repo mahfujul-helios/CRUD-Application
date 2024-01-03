7a49960521273cdf991bafc579aa63c59793c4e3
mahfujur <mahfujul.hoque@heliosinsider.com>
2024-01-03 10:22:40 +0600

    add-docekr

--- students/settings.py
+++ students/settings.py
@@ -112,7 +112,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+ALLOWED_HOSTS = ["*"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
